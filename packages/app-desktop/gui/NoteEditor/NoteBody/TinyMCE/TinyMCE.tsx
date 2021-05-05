e72c45c2c722a888de315c637d792ba5bf494e46
jun.kobayashi <jun.kobayashi@access-company.com>
2021-05-06 08:21:51 +0900

    add my plugin

--- packages/app-desktop/gui/NoteEditor/NoteBody/TinyMCE/TinyMCE.tsx
+++ packages/app-desktop/gui/NoteEditor/NoteBody/TinyMCE/TinyMCE.tsx
@@ -551,8 +551,68 @@ const TinyMCE = (props: NoteBodyEditorProps, ref: any) => {
 				'h1', 'h2', 'h3', 'hr', 'blockquote', 'table', `joplinInsertDateTime${toolbarPluginButtons}`,
 				'|', 'fontselect', 'fontsizeselect', 'formatselect',
 				'|', 'forecolor', 'backcolor', 'casechange', 'permanentpen', 'formatpainter', 'removeformat',
-				'|', 'toc',
+				'|', 'toc', 'example',
 			];
+
+			(window as any).tinymce.PluginManager.add('example', function(editor: any, _url: string) {
+				const openDialog = function() {
+				  return editor.windowManager.open({
+						title: 'Example plugin',
+						body: {
+					  type: 'panel',
+					  items: [
+								{
+						  type: 'input',
+						  name: 'title',
+						  label: 'Title',
+								},
+					  ],
+						},
+						buttons: [
+					  {
+								type: 'cancel',
+								text: 'Close',
+					  },
+					  {
+								type: 'submit',
+								text: 'Save',
+								primary: true,
+					  },
+						],
+						onSubmit: function(api: any) {
+					  const data = api.getData();
+					  /* Insert content when the window form is submitted */
+					  editor.insertContent(`Title: ${data.title}`);
+					  api.close();
+						},
+				  });
+				};
+				/* Add a button that opens a window */
+				editor.ui.registry.addButton('example', {
+				  text: 'My button',
+				  onAction: function() {
+					/* Open window */
+						openDialog();
+				  },
+				});
+				/* Adds a menu item, which can then be included in any menu via the menu/menubar configuration */
+				editor.ui.registry.addMenuItem('example', {
+				  text: 'Example plugin',
+				  onAction: function() {
+					/* Open window */
+						openDialog();
+				  },
+				});
+				/* Return the metadata for the help plugin */
+				return {
+				  getMetadata: function() {
+						return {
+					  name: 'Example plugin',
+					  url: 'http://exampleplugindocsurl.com',
+						};
+				  },
+				};
+			  });
 
 			const editors = await (window as any).tinymce.init({
 				selector: `#${rootIdRef.current}`,
@@ -564,7 +624,7 @@ const TinyMCE = (props: NoteBodyEditorProps, ref: any) => {
 				resize: false,
 				icons: 'Joplin',
 				icons_url: 'gui/NoteEditor/NoteBody/TinyMCE/icons.js',
-				plugins: 'noneditable link joplinLists hr searchreplace codesample table toc',
+				plugins: 'noneditable link joplinLists hr searchreplace codesample table toc example',
 				noneditable_noneditable_class: 'joplin-editable', // Can be a regex too
 				valid_elements: '*[*]', // We already filter in sanitize_html
 				menubar: false,
