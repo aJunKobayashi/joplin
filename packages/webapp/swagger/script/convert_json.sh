3a2702b86abe6f66dd178f5ec20a9c4267af2cfa
jun.kobayashi <jun.kobayashi@access-company.com>
2021-09-13 13:06:16 +0900

    add swagger ui

---
+++ packages/webapp/swagger/script/convert_json.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# shellcheck shell=bash
+set -eu
+
+gSwaggerFile="swagger.yaml"
+gSwaggerPath="../yaml/${gSwaggerFile}"
+gTmpFolder="./tmp"
+gDataJs="data.js"
+
+main() {
+  echo "converting swagger.yaml to json...."
+
+  set +eu
+  mkdir "${gTmpFolder}"
+  set -eu
+
+  cp -f "${gSwaggerPath}" "${gTmpFolder}"
+  docker run -v "$(pwd)/${gTmpFolder}:/docs/tmp" swaggerapi/swagger-codegen-cli generate -i /docs/tmp/${gSwaggerFile} -l swagger -o /docs/tmp
+  #   cp -f "${gTmpFolder}/swagger.json" "."
+  echo "restAPIDefinition = " >"${gDataJs}"
+  cat "${gTmpFolder}/swagger.json" >>"./html/${gDataJs}"
+  mv "${gDataJs}" ../${gDataJs}
+  rm -rf "${gTmpFolder}"
+}
+main
