3794ffcc458efc7af750178c8115e3339276cbb5
jun.kobayashi <jun.kobayashi@access-company.com>
2023-12-25 09:28:26 +0900

    fix bug that image is not shown

--- packages/app-desktop/gui/MainScreen/commands/exportMergedPdf.ts
+++ packages/app-desktop/gui/MainScreen/commands/exportMergedPdf.ts
@@ -4,6 +4,8 @@ import { CommandRuntime, CommandDeclaration, CommandContext } from '@joplin/lib/
 import { _ } from '@joplin/lib/locale';
 import { createMergedNoteBody } from './mergeNotes';
 import Folder from '@joplin/lib/models/Folder';
+import { copyPluginAssetsIfNotExit, modifyJoplinResourceAndSetPlugin } from '../../../commands/showBrowser';
+import Setting from '@joplin/lib/models/Setting';
 const bridge = require('electron').remote.require('./bridge').default;
 // import Note from '@joplin/lib/models/Note';
 // const bridge = require('electron').remote.require('./bridge').default;
@@ -17,7 +19,6 @@ export const runtime = (comp: any): CommandRuntime => {
 	return {
 		execute: async (__: CommandContext, folderId: string) => {
 			console.log(`folderId: ${folderId}`);
-			const mergedNoteBody = await createMergedNoteBody(folderId);
 			let path = null;
 			const folder = await Folder.load(folderId);
 			path = bridge().showSaveDialog({
@@ -27,8 +28,13 @@ export const runtime = (comp: any): CommandRuntime => {
 
 
 			if (!path) return;
+			const resourceDir = `${Setting.value('resourceDir')}`;
+			const mergedNoteBody = await createMergedNoteBody(folderId);
+			await copyPluginAssetsIfNotExit();
+			const htmlBody = await modifyJoplinResourceAndSetPlugin(mergedNoteBody,resourceDir);
+
 			const pdfPath = path;
-			await comp.printTo_('pdf', { path: pdfPath, noteId: null, htmlBody: mergedNoteBody });
+			await comp.printTo_('pdf', { path: pdfPath, noteId: null, htmlBody: htmlBody });
 
 		},
 
