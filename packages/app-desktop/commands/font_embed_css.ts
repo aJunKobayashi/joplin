edca3ef5bddbd57f0baddef443eecf23009f9f69
jun.kobayashi <jun.kobayashi@access-company.com>
2023-12-25 22:32:06 +0900

    fix path again

--- packages/app-desktop/commands/font_embed_css.ts
+++ packages/app-desktop/commands/font_embed_css.ts
@@ -19,43 +19,48 @@ const encodeFont = (fontPath: string): string | null => {
 };
 
 
+// customPluginをファクトリ関数として定義
+const createCustomPlugin = (cssFilepath: string): postcss.Plugin => {
+	return {
+		postcssPlugin: 'custom-plugin',
+		Once(root) {
+			root.walkAtRules('font-face', rule => {
+				rule.walkDecls('src', decl => {
+					console.log(`decl.value: ${decl.value}`);
 
-const customPlugin: postcss.Plugin = {
-	postcssPlugin: 'custom-plugin',
-	Once(root) {
-		root.walkAtRules('font-face', rule => {
-			rule.walkDecls('src', decl => {
-				console.log(`decl.value: ${decl.value}`);
+					const urlRegex = /url\((.*?)\)/g;
+					const urls = decl.value.match(urlRegex)?.map(match => {
+						const urlMatch = /url\((['"]?)(.*?)\1\)/.exec(match);
+						return urlMatch ? urlMatch[2] : null;
+					}).filter(url => url !== null) as string[];
 
-				const urlRegex = /url\((.*?)\)/g;
-				const urls = decl.value.match(urlRegex)?.map(match => {
-					const urlMatch = /url\((['"]?)(.*?)\1\)/.exec(match);
-					return urlMatch ? urlMatch[2] : null;
-				}).filter(url => url !== null) as string[];
-
-				const formatRegex = /format\((.*?)\)/g;
-				const formats = decl.value.match(formatRegex)?.map(match => {
-					const formatMatch = /format\((['"]?)(.*?)\1\)/.exec(match);
-					return formatMatch ? formatMatch[2] : null;
-				}).filter(format => format !== null) as string[];
+					const formatRegex = /format\((.*?)\)/g;
+					const formats = decl.value.match(formatRegex)?.map(match => {
+						const formatMatch = /format\((['"]?)(.*?)\1\)/.exec(match);
+						return formatMatch ? formatMatch[2] : null;
+					}).filter(format => format !== null) as string[];
 
 
 
-				console.log(`urls: ${urls}`);
-				console.log(`formats: ${formats}`);
-				const newValues = urls?.map((url, index) => {
-					const fontPath = path.resolve('./test_file', url);
-					const newValue = `url(${encodeFont(fontPath)}) format('${formats[index]}')`;
-					return newValue;
+					console.log(`urls: ${urls}`);
+					console.log(`formats: ${formats}`);
+					const newValues = urls?.map((url, index) => {
+						const fontPath = path.join(dirname(cssFilepath), url);
+						const newValue = `url(${encodeFont(fontPath)}) format('${formats[index]}')`;
+						return newValue;
+					});
+					decl.value = newValues?.join(', ') || decl.value;
 				});
-				decl.value = newValues?.join(', ') || decl.value;
 			});
-		});
-	},
+		},
+	};
 };
+
+
 
 export const createEmbededFontCss = async (cssFilePath: string, outputPath: string): Promise<string> => {
 	const inputCss = fs.readFileSync(cssFilePath, 'utf8');
+	const customPlugin = createCustomPlugin(cssFilePath);
 	const result = await postcss([customPlugin]).process(inputCss, { from: cssFilePath, to: outputPath });
 	return result.css;
 };
