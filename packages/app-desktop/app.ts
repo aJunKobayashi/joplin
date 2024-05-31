e837b4e02b2cd11d0253258ebd9b64a6cf3a9da2
jun.kobayashi <jun.kobayashi@access-company.com>
2024-05-31 15:58:04 +0900

    electron-is-devを削除

--- packages/app-desktop/app.ts
+++ packages/app-desktop/app.ts
@@ -588,11 +588,10 @@ class Application extends BaseApplication {
 	}
 
 	async start(argv: string[]): Promise<any> {
-		const electronIsDev = require('electron-is-dev');
 
 		// If running inside a package, the command line, instead of being "node.exe <path> <flags>" is "joplin.exe <flags>" so
 		// insert an extra argument so that they can be processed in a consistent way everywhere.
-		if (!electronIsDev) argv.splice(1, 0, '.');
+		if (!bridge().electronIsDev()) argv.splice(1, 0, '.');
 
 		argv = await super.start(argv);
 
