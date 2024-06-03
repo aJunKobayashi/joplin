cf3e10d84b9c3b11e22daaf3a81162355f99f47f
jun.kobayashi <jun.kobayashi@access-company.com>
2024-06-03 15:55:11 +0900

    余計なスタイルを削除するようにしたが、うまくいかない

--- packages/renderer/assets/mathjax/mathjax_render.js
+++ packages/renderer/assets/mathjax/mathjax_render.js
@@ -1,8 +1,24 @@
 
 
+document.addEventListener('joplin-mathJaxLoad', (event) => {
+    console.log(`fix mathjax heaer`)
+    MathJax = {
+        tex: {
+            packages: { '[+]': ['ams'] },
+            inlineMath: [['$', '$'], ['\\(', '\\)']],
+            displayMath: [['$$', '$$'], ['\\[', '\\]']],
+        },
+        chtml: {
+            // カスタムスタイルを空に設定してデフォルトスタイルを無効にする
+            styles: {}
+        }
+    };
+});
+
+
 
 document.addEventListener('joplin-mathJaxUpdate', (event) => {
-	console.log(`joplin-mathJaxUpdate event triggered!`);
+    console.log(`joplin-mathJaxUpdate event triggered!`);
     const targetId = event.detail.id;
     const target = document.getElementById(targetId);
     MathJax.typesetPromise([target]);
