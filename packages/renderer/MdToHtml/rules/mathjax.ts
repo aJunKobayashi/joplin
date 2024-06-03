f31f0676bf6a8c6d43e209c41705def11ef0db0d
jun.kobayashi <jun.kobayashi@access-company.com>
2024-06-03 15:58:03 +0900

    フォントの表示がおかしくなる問題を修正

--- packages/renderer/MdToHtml/rules/mathjax.ts
+++ packages/renderer/MdToHtml/rules/mathjax.ts
@@ -2,7 +2,7 @@ export default {
 
 	assets: function() {
 		return [
-			{ name: 'tex-mml-chtml.js' },
+			{ name: 'tex-mml-chtml.js?config=TeX-MML-AM_CHTML' },
 			{ name: 'mathjax_render.js' },
 		];
 	},
