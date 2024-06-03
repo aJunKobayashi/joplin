a4d4f770cf88e8445a29a10c1c8f0812cea3c526
jun.kobayashi <jun.kobayashi@access-company.com>
2024-06-04 08:34:56 +0900

    htmlをエクスポートできなくなった問題を修正

--- packages/renderer/MdToHtml/rules/mathjax.ts
+++ packages/renderer/MdToHtml/rules/mathjax.ts
@@ -6,4 +6,13 @@ export default {
 			{ name: 'tex-mml-chtml.js' },
 		];
 	},
+	plugin: function(markdownIt: any) {
+		const defaultRender: Function = markdownIt.renderer.rules.fence || function(tokens: any[], idx: number, options: any, env: any, self: any) {
+			return self.renderToken(tokens, idx, options, env, self);
+		};
+
+		markdownIt.renderer.rules.fence = function(tokens: any[], idx: number, options: {}, env: any, self: any) {
+			return defaultRender(tokens, idx, options, env, self);
+		};
+	},
 };
