134f529f444a099b7c4d834f6d56efce32f53b11
jun.kobayashi <jun.kobayashi@access-company.com>
2024-06-05 15:47:03 +0900

    数式を左寄せに変更

--- packages/renderer/assets/katex/katex_rendrer.js
+++ packages/renderer/assets/katex/katex_rendrer.js
@@ -6,6 +6,11 @@ document.addEventListener('joplin-kartexUpdate', (event) => {
     const fontSize = event.detail.fontSize;
     const target = document.getElementById(targetId);
     renderMathInElement(target);
+    const elements = target.querySelectorAll(`.katex-display>.katex `);
+    for(let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+        element.style.setProperty('text-align', 'left', 'important');
+    }
     target.firstChild.style.fontSize = `${fontSize}px`;
     console.log(`fiinished typesetting!. id: ${targetId}`);
 });
