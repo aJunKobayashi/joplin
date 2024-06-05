fa266a258aecd7ffa6fc7e3bac04b0d45b1c02ed
jun.kobayashi <jun.kobayashi@access-company.com>
2024-06-05 09:35:59 +0900

    tex_svg.jsを利用するように修正

--- packages/renderer/assets/mathjax/mathjax_render.js
+++ packages/renderer/assets/mathjax/mathjax_render.js
@@ -11,8 +11,9 @@ document.addEventListener('joplin-mathJaxUpdate', (event) => {
     const fontSize = event.detail.fontSize;
     const target = document.getElementById(targetId);
     const tex = target.innerText;
-    const mml = MathJax.tex2mml(tex, {display: false});
-    target.innerHTML = mml;
+    const container = MathJax.tex2svg(tex, { display: false });
+    const [svg] = container.children;
+    target.appendChild(svg);
     const mathTag = target.firstChild;
     mathTag.style.fontSize = `${fontSize}px`;
     console.log(`fiinished typesetting!. id: ${targetId}`);
