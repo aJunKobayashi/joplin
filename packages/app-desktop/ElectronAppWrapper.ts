48d0aa1d303504ccea10a4e805b6ac8d354af76c
jun.kobayashi <jun.kobayashi@access-company.com>
2024-12-16 12:15:59 +0900

    disable fullscreen mode

--- packages/app-desktop/ElectronAppWrapper.ts
+++ packages/app-desktop/ElectronAppWrapper.ts
@@ -94,7 +94,7 @@ export default class ElectronAppWrapper {
 			// We start with a hidden window, which is then made visible depending on the showTrayIcon setting
 			// https://github.com/laurent22/joplin/issues/2031
 			show: debugEarlyBugs,
-			fullscreen: true,
+			fullscreen: false,
 		};
 
 		// Linux icon workaround for bug https://github.com/electron-userland/electron-builder/issues/2098
