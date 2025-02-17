ce13ddeadfeee294b188d33e9a191fee903277d4
jun.kobayashi <jun.kobayashi@access-company.com>
2025-02-17 16:46:21 +0900

    load cheerio in webworker

---
+++ packages/app-desktop/WebWorker.js
@@ -0,0 +1,30 @@
+const cheerio = require('cheerio');
+
+const revertResourceDirToJoplinScheme = (htmlBody, resourceDir) => {
+	const $ = cheerio.load(htmlBody);
+	const anchors = [...$(`a[href^="file://${resourceDir}"]`), ...$(`a[href^="${resourceDir}"]`)];
+	for (let i = 0; i < anchors.length; i++) {
+		const anchor = anchors[i] ;
+		const href = anchor.attribs.href;
+		const filename = PATH.basename(href);
+		const newHref = `joplin_resource://${filename}`;
+		anchor.attribs.href = newHref;
+	}
+
+	const imgs = [...$(`img[src^="file://${resourceDir}"]`), ...$(`img[src^="${resourceDir}"]`)];
+	for (let i = 0; i < imgs.length; i++) {
+		const img = imgs[i];
+		const src = img.attribs.src;
+		const filename = PATH.basename(src);
+		const newSrc = `joplin_resource://${filename}`;
+		img.attribs.src = newSrc;
+	}
+	return $;
+};
+
+self.onmessage = function (e) {
+    console.log(`work received: ${JSON.stringify(e.data, null, 2)}`);
+    const newData = revertResourceDirToJoplinScheme(e.data.md, e.data.resourceDir).html();
+    self.postMessage(newData);
+};
+//# sourceMappingURL=WebWorker.js.map
