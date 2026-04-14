9e056b3373606064199695444999f6dc28ee4ad7
jun.kobayashi <jun.kobayashi@access-company.com>
2026-04-14 11:31:21 +0900

    fix build error  with electron stub

--- packages/web_editor/next.config.ts
+++ packages/web_editor/next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverExternalPackages: ['faiss-node', 'keytar', 'sharp'],
+  serverExternalPackages: ['faiss-node', 'keytar', 'sharp', '@electron/remote'],
+  turbopack: {
+    resolveAlias: {
+      electron: './stubs/electron.js',
+    },
+  },
 };
 
 export default nextConfig;
